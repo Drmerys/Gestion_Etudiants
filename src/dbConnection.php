5270d8ab42d227d9a86fe42a9020ce26bdefc3e9
Drmerys <bakisdu95@hotmail.fr>
2020-04-09 22:56:24 +0200

    db connection

--- src/dbConnection.php
+++ src/dbConnection.php
@@ -1,6 +1,6 @@
 <?php
   try {
-    $bdd = new PDO('mysql:host=localhost;port=3308;dbname=gestion_etudiants;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=gestion_etudiants;charset=utf8', 'projet', 'iutv');
   } catch (Exception $e) {
     die('Erreur lors de la connexion : ' . $e->getMessage());
   }
